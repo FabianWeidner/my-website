9a6f16434e41e20b860242d7fa71afb0b576d0c8
Fabian Phillipe <fabianphillipe@gmail.com>
2021-10-07 23:19:59 +0200

    start with styling for my new website

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div>
+      <div className="header__backgroundImg"></div>
+    </div>
+  );
+}
+
+export default Header;
